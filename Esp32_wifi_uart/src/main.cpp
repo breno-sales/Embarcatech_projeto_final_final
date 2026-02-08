4d75dc9a162708f93f59ad23bc407d5c1afe0de1
breno-sales <25sales.breno@gmail.com>
2026-02-08 18:12:30 -0300

    08/02 - Organização na estrutura do projeto e renomeação

---
+++ Esp32_wifi_uart/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "functions.h"
+#include "variaveis_globais.h"
+
+int cont = 0;
+
+
+void setup() {
+    Serial.begin(115200);
+    Serial2.begin(UART2_BAUD, SERIAL_8N1, UART2_RX, UART2_TX);
+    delay(3000);
+    
+    iniciando_wifi();
+    
+
+}
+
+/* ================= LOOP ================= */
+
+void loop() {
+
+    //====================== CONEXAO TCP ============================
+    execucao_wifi_em_loop();
+
+    // ======================== RECEBIMENTO DE MSG UART SEM CONEXAO COM TCP =============================
+    if (uart_received_buffer(msg, sizeof(msg))) {
+        Serial.printf("\nMensagem recebida UART sem conexao com TCP: %s\n", msg);
+    }
+
+    
+}
+
